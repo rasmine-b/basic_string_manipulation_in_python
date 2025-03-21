e60de4361ca9c0f4f26d6f78979b1b0f59222c5c
rasmine <blasrasmine@gmail.com>
2025-03-21 15:18:18 +0800

    pseudocode

---
+++ converts_fullname_to_proper_casing.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+#Ask the user for input
+#Convert to proper casing and print the result
