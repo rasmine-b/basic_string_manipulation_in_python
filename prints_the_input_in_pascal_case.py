b2a9e12f3bcc2933d1433ebe09ea8f55531d82c5
rasmine <blasrasmine@gmail.com>
2025-03-21 15:35:10 +0800

    pseudocode

---
+++ prints_the_input_in_pascal_case.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+#Ask the user for input
+#Convert to pascal Case and print the result
