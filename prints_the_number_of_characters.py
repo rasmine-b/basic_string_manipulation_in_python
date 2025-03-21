d8002510fe198e0c7b1cbd4d01b6e18e4ebfea3f
rasmine <blasrasmine@gmail.com>
2025-03-21 15:32:01 +0800

    pseudocode

---
+++ prints_the_number_of_characters.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+#Ask user for input
+#Count the number of characters and print the result
