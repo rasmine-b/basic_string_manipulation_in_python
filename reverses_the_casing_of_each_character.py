804721dbf97407415b1c1f214568bfd8a5c1ff02
rasmine <blasrasmine@gmail.com>
2025-03-21 15:23:43 +0800

    pseudocode

---
+++ reverses_the_casing_of_each_character.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#Ask user for input
+#Reverse the casing and print the result
