b42c3c3156039e3689342fba8c1f5baaf3b1b4b3
rasmine <blasrasmine@gmail.com>
2025-03-21 15:03:41 +0800

    pseudocode

---
+++ formats_number_as_a_6_digit_string.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+#Ask the user for input
+#Format the number as a 6-digit string and print the result
