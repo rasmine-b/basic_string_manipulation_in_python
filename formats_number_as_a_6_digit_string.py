3a3e327fcf2e464f9b00239d6af2c7b09f7bef9a
rasmine <blasrasmine@gmail.com>
2025-03-21 15:04:44 +0800

    ask user for input

--- formats_number_as_a_6_digit_string.py
+++ formats_number_as_a_6_digit_string.py
@@ -1,4 +1,6 @@
 #Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 #Ask the user for input
+num = int(input("Enter a number: "))
+
 #Format the number as a 6-digit string and print the result
