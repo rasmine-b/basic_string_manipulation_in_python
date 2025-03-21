f623522bd21334bc442d55d4045d50b888c65294
rasmine <blasrasmine@gmail.com>
2025-03-21 15:35:44 +0800

    ask the user for input

--- prints_the_input_in_pascal_case.py
+++ prints_the_input_in_pascal_case.py
@@ -1,4 +1,6 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 #Ask the user for input
+fullname = input("Enter your full name with incorrect casing: ")
+
 #Convert to pascal Case and print the result
