df60af679fa8728f6f9884e96f894323461d3159
rasmine <blasrasmine@gmail.com>
2025-03-21 15:29:05 +0800

    pseudocode

---
+++ prints_the_number_of_words.py
@@ -0,0 +1,4 @@
+#Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+#Ask user for input
+#Split the statement into words and count them and print the result
