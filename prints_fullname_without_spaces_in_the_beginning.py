fa171a0d153d8c62a8275f545b2a69e1e8557807
rasmine <blasrasmine@gmail.com>
2025-03-21 14:57:10 +0800

    Updated commit message

--- prints_fullname_without_spaces_in_the_beginning.py
+++ prints_fullname_without_spaces_in_the_beginning.py
@@ -1,6 +1,6 @@
 #Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 #Ask the user for input
-fullname = input("Enter your fullname: ")
+fullname = input("Enter your fullname with leading spaces: ")
 
 #Remove leading spaces and print the result
