5cd421c9d86a356f0465243f9b358e9493e5ce2e
rasmine <blasrasmine@gmail.com>
2025-03-21 15:11:28 +0800

    ask user for input

--- prints_input_in_all_capital_letters.py
+++ prints_input_in_all_capital_letters.py
@@ -1,4 +1,6 @@
 #Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 #Ask user for input
+fullname = input("Enter your fullname: ")
+
 #Change to uppercase and print the result
