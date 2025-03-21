0f280ffb518b7d4d0b2eeb4d8d994e67a85a0220
rasmine <blasrasmine@gmail.com>
2025-03-21 15:36:32 +0800

    convert to pascal case and print the result

--- prints_the_input_in_pascal_case.py
+++ prints_the_input_in_pascal_case.py
@@ -4,3 +4,5 @@
 fullname = input("Enter your full name with incorrect casing: ")
 
 #Convert to pascal Case and print the result
+pascal_case_name = fullname.title().replace(" ", "")
+print("Your name in Pascal Case:", pascal_case_name)
