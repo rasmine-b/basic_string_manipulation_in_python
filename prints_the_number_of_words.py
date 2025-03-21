90cd0b82d57ada639c80a5e50d4c7488c3fe443f
rasmine <blasrasmine@gmail.com>
2025-03-21 15:29:46 +0800

    ask user for input

--- prints_the_number_of_words.py
+++ prints_the_number_of_words.py
@@ -1,4 +1,6 @@
 #Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 #Ask user for input
+statement = input("Enter a complete statement: ")
+
 #Split the statement into words and count them and print the result
